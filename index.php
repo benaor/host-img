13d3203f18be7e98d25c2c3fe385e42f647e9c64
benaor <benjamingirard25@gmail.com>
2020-08-10 23:07:04 +0200

    header

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+//  ICI LE CODE PHP 
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>image host</title>
+
+    <!-- BOOTSTRAP CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+
+<body>
+    <header class="container-fluid bg-primary">
+        <div class="container p-5 m-auto">
+            <h1 class="text-white text-center"> Hebergeur d'image </h1>
+        </div>
+    </header>
+
+</body>
+
+</html>
